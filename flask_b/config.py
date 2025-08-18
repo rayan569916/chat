1d6e7b19e7a9ac5f771866eb3edb630388719136
POLUS/abin_raj_b <abin.r@Polussolutions.com>
2025-08-18 18:19:35 +0530

    Fix model import and class name in app.py

--- flask_b/config.py
+++ flask_b/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg2://root:root@192.168.1.6:5432/mydb")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:root@localhost:3306/chat_schema")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
--- flask_b/flask_app/route/test_route.py
+++ flask_b/flask_app/route/test_route.py
@@ -6,11 +6,8 @@ test_route = Blueprint('test_route', __name__)
 
 @test_route.route('/test-db-connectivity', methods=['GET'])
 def test_db_connectivity():
-    print("Route /test-db-connectivity called")
     try:
         db.session.execute(text('SELECT 1;'))
-        print("Query executed successfully") 
         return "Database connection is successful", 200
     except Exception as e:
-        print(f"Database connection failed: {str(e)}")
         return f"Database connection failed: {str(e)}", 500
